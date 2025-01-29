2ac1514c79bf60e06aa5659c2abdb8469d7ec672
Vydra95 <ekaterina4an@gmail.com>
2025-01-29 22:32:24 +0700

    added

--- Topic3/Extra_task_1.py
+++ Topic3/Extra_task_1.py
@@ -1,34 +1,21 @@
 """Второй по величине элемент последовательности"""
 
-numbers = [21, 16, 7, 5, 0]
+numbers = [16, 21, 7, 5, 32, 0]
 
-# Версия "Какая-то херь, но работает"
-x = 1
-found = False
+max1 = numbers[0]
+max2 = numbers[1]
 
-while not found:
-    for num in range(len(numbers)):
-        if x != numbers[num] and num < len(numbers):
-            num += 1
-        else:
-            found = True
-            print(x)
-    x += 1
+if max2 > max1:
+    max1, max2 = max2, max1
 
+i = 2
+while i < len(numbers):
+    if numbers[i] > max1:
+        max2 = max1
+        max1 = numbers[i]
+    elif numbers[i] > max2:
+        max2 = numbers[i]
 
-# Версия "Ну типа норм"
-x1 = numbers[0]
-x2 = numbers[1]
-a = 1
+    i += 1
 
-while a < (len(numbers) - 1):
-    if x1 < x2:
-        numbers[0] = x1
-    else:
-        numbers[0] = x2
-        numbers[1] = x1
-        x1 = numbers[0]
-    x2 = numbers[a + 1]
-    a += 1
-
-print(x1)
+print(max2)
--- Topic3/Extra_task_3.py
+++ Topic3/Extra_task_3.py
@@ -1,17 +1,33 @@
-"""Сколько идущих подряд элементов равны друг другу?"""
+"""Какое максимальное число идущих подряд равных друг другу элементов?"""
 
-numbers = [16, 21, 21, 21, 7, 7, 85, 79, 79, 79, 0]
-a = 0
-counter = 0
+numbers = [
+    16,
+    21,
+    21,
+    7,
+    7,
+    85,
+    79,
+    79,
+    79,
+    0,
+]
+a = 1
+maxx = 1
+counter = 1
 
-while a < (len(numbers) - 2):
-    if numbers[a] != numbers[a + 1] and numbers[a + 1] == numbers[a + 2]:
-        counter += 2
-        a += 1
-    elif numbers[a] == numbers[a + 1] and numbers[a + 1] == numbers[a + 2]:
+while a < len(numbers):
+    if numbers[a] == numbers[a - 1]:
         counter += 1
-        a += 1
     else:
-        a += 1
+        if counter > maxx:
+            maxx = counter
 
-print(counter)
+        counter = 1
+
+    a += 1
+
+if counter > maxx:
+    maxx = counter
+
+print(maxx)
